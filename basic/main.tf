a9dcccce9b438c217a9571800eee5ce8a5186df4
Nitin Sharma <nitinsh3101@gmail.com>
2024-01-28 20:28:03 +0530

    update instance type

--- basic/main.tf
+++ basic/main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 
 resource "aws_instance" "app_server" {
   ami = "ami-02f3f602d23f1659d" // AWS AMI 2023 machine
-  instance_type = "t2.micra"
+  instance_type = "t2.medium"
   tags = {
     Name = "first-tf-machine"
   }
